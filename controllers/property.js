04143a98a63f88f443a2f1ed086170607126cca2
Eleanor Latus <eleanor@latus.co.uk>
2023-02-13 08:54:51 +0000

    removed some unruly ejs

--- controllers/property.js
+++ controllers/property.js
@@ -1,5 +1,6 @@
 const Review = require("../models/Review.js");
 const Property = require("../models/Property.js");
+const User = require("../models/User.js");
 
 module.exports = {
     getProperty: async (req, res) => {
@@ -7,7 +8,8 @@ module.exports = {
         const property = await Property.findById(req.params.id);
         console.log(property._id)
         const review = await Review.find({ propertyId: req.params.id}).sort({ createdAt: "desc" }).lean();
-        res.render("property.ejs", { property: property, review: review, user: req.user});
+        res.render("property.ejs", { property: property, review: review});
+        console.log(user)
         } catch (err) {
           console.log(err);
         }
