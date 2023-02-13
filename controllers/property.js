08a5b37df284a68eb94429478bcd3a8137f22b2d
Eleanor Latus <eleanor@latus.co.uk>
2023-02-13 09:31:55 +0000

    fixed footer to bottom on log in page

--- controllers/property.js
+++ controllers/property.js
@@ -1,6 +1,5 @@
 const Review = require("../models/Review.js");
 const Property = require("../models/Property.js");
-const User = require("../models/User.js");
 
 module.exports = {
     getProperty: async (req, res) => {
@@ -8,8 +7,8 @@ module.exports = {
         const property = await Property.findById(req.params.id);
         console.log(property._id)
         const review = await Review.find({ propertyId: req.params.id}).sort({ createdAt: "desc" }).lean();
-        res.render("property.ejs", { property: property, review: review});
-        console.log(user)
+        res.render("property.ejs", { property: property, review: review, user: req.user});
+        console.log(req.user)
         } catch (err) {
           console.log(err);
         }
