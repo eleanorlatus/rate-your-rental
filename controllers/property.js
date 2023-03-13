1958e118251ed414905208d551a408050bcda69f
Eleanor Latus <eleanor@latus.co.uk>
2023-03-13 15:56:50 +0000

    search bar on index works for properties

--- controllers/property.js
+++ controllers/property.js
@@ -12,4 +12,15 @@ module.exports = {
           console.log(err);
         }
       },
+      searchProperty: async (req, res) => {
+        try {
+        console.log(req.query)
+        const property = await Property.findOne({streetName : req.query.property})
+        const review = await Review.find({ propertyId: property._id}).populate("user").sort({ tenancyFrom: "desc" }).lean();
+        console.log(property)
+        res.render("property.ejs", { property: property, review: review, loggedInUser: req.user});
+        } catch (err) {
+          console.log(err);
+        }
+      },
   };
--- routes/property.js
+++ routes/property.js
@@ -6,4 +6,6 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Comment Routes - simplified for now
 router.get("/:id", propertyController.getProperty);
 
+router.get("/search/property", propertyController.searchProperty);
+
 module.exports = router;
