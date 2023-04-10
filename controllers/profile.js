19c2fe469ba99468bcbfe1f2fe266fac8377871a
Eleanor Latus <eleanor@latus.co.uk>
2023-04-10 15:58:00 +0100

    made the account deletion more comprehensive, mirrors the delete review method

--- controllers/profile.js
+++ controllers/profile.js
@@ -84,15 +84,28 @@ module.exports = {
         }
       },
       deleteProfile: async (req, res) => {
-        try {
+        try {          
+          const reviews = await Review.find({ user: req.user._id });
+          // loop through and delete each review
+          for (const review of reviews){
+          await cloudinary.uploader.destroy(review.cloudinaryId);
+          await Review.deleteOne({ _id: review.id });
+
+          // update property document
+          await Property.updateOne({_id: review.propertyId}, { $pull: { images: { reviewId: review.id }} });
+          await Property.updateOne({_id: review.propertyId}, { $pull: { reviews: review.id } });
+          const property = await Property.findOne({ _id: review.propertyId });
+        
+          // if a property no longer holds a review, delete said property
+          if(property.reviews.length == 0){
+            await Property.deleteOne({ _id: review.propertyId });
+          }}
+
           // delete the user's profile
-          const user = await User.findOne({userName: req.user.userName}).lean();
-          await User.deleteOne({ _id: user._id });
+          await User.deleteOne({ _id: req.user._id });
 
-          // delete all corresponding reviews
-          await Review.deleteMany({ user: user._id });
-
-          console.log(`${user.userName} and all connected reviews have been sucessfully deleted`)
+          console.log(`${req.user.userName}'s profile and all connected reviews have been sucessfully deleted`)
+         
           // redirect to homepage
           res.redirect("/");
         } catch (err) {
