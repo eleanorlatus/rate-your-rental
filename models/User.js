73377ec8d70c2f24c739177a5752757f95e1b7ac
Eleanor Latus <eleanor@latus.co.uk>
2023-04-10 11:57:03 +0100

    updated default profile photo on creation of new accounts

--- models/User.js
+++ models/User.js
@@ -5,8 +5,8 @@ const UserSchema = new mongoose.Schema({
   userName: { type: String, unique: true },
   email: { type: String, unique: true },
   password: String,
-  profilePhoto: {type: String, default: "https://res.cloudinary.com/di1ru6w5x/image/upload/v1676130247/cat_i0yavv.jpg"},
-  cloudinaryId: {type:String, default: "cat_i0yavv"}
+  profilePhoto: {type: String, default: "https://res.cloudinary.com/di1ru6w5x/image/upload/v1681123931/ekqv0wmzb9rgw4dthzle.png"},
+  cloudinaryId: {type:String, default: "ekqv0wmzb9rgw4dthzle"}
 });
 
 // Password hash middleware.
